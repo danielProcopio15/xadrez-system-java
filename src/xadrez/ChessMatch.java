07aa4b07baa7b0702d078b949cf6805630c79fdb
danielProcopio15 <danielprocopio15@gmail.com>
2024-04-19 19:11:29 -0300

    adicionei cores e novas peças no jogo

--- src/xadrez/ChessMatch.java
+++ src/xadrez/ChessMatch.java
@@ -35,9 +35,19 @@ public class ChessMatch {
 
 
     private void initialSetup(){
-        PlaceNewPiece('B', 6,new Torre(board, Color.WHITE));
-        PlaceNewPiece('E', 8, new Rei(board, Color.BLACK));
+        PlaceNewPiece('C', 1, new Torre(board, Color.WHITE));
+        PlaceNewPiece('C', 2, new Torre(board, Color.WHITE));
+        PlaceNewPiece('D', 2, new Torre(board, Color.WHITE));
+        PlaceNewPiece('E', 2, new Torre(board, Color.WHITE));
+        PlaceNewPiece('E', 1, new Torre(board, Color.WHITE));
         PlaceNewPiece('D', 1, new Rei(board, Color.WHITE));
+
+        PlaceNewPiece('C', 7, new Torre(board, Color.BLACK));
+        PlaceNewPiece('C', 8, new Torre(board, Color.BLACK));
+        PlaceNewPiece('D', 7, new Torre(board, Color.BLACK));
+        PlaceNewPiece('E', 7, new Torre(board, Color.BLACK));
+        PlaceNewPiece('D', 8, new Torre(board, Color.BLACK));
+        PlaceNewPiece('E', 8, new Rei(board, Color.BLACK));
     }
 
 }
